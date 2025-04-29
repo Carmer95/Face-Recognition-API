d877bed57a0faabc17192d02ce1db59909374af7
Dakota <carmer95@gmail.com>
2025-04-29 17:00:28 -0400

    Initial late commit, fully built with database connectivity, barring 4-29 fix of db to trx. Pushing to represent the difference.

---
+++ server.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors');
+const knex = require('knex');
+
+const app = express();
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1', //localhost
+      user : 'postgres', //add your user name for the database here
+      port: 5432, // add your port number here
+      password : 'P0stgr3s!', //add your correct password in here
+      database : 'smart-brain' //add your database name you created here
+    }
+});
+
+// db.select('*').from('users').then(data => {
+//     console.log(data);
+// });
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'apples',
+            entries: 0,
+            joined: new Date(),
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date(),
+        },
+    ]
+}
+
+app.use(bodyParser.json());
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email && 
+	    req.body.password === database.users[0].password) {
+        res.json(database.users[0]);
+        console.log(database.users[0]);
+	} else {
+		res.status(400).json('Error logging in.')
+	}
+})
+
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+		trx.insert({
+			hash: hash,
+			email: email
+		})
+        .into('login')
+        .returning('email')
+        .then(loginEmail => {
+            return db('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0].email,
+                    name: name,
+                    joined: new Date()
+                })
+                .then(user => {
+                    res.json(user[0]);
+                })
+		})
+        // .then(trx.commit)
+        // .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('Unable to register.'))
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    db.select('*').from('users').where({id})
+        .then(user => {	
+            if (user.length) {
+			res.json(user[0])
+		} else {
+			res.status(400).json('Error getting user: Not found.')
+        }
+    })
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+	.increment('entries', 1)
+	.returning('entries')
+	.then(entries => {
+		res.json(entries[0].entries)
+	})
+    .catch(err => res.status(400).json('Unable to get entries.'))
+})
+
+app.listen(3001, () => {
+    console.log('App is running on Port 3001');
+});
+
+/*
+	/ (root) --> res = this is working
+	/signin --> POST = success/fail
+	/register --> POST = user
+	/profile/:userId --> GET = user
+    /image --> PUT --> user
+*/
